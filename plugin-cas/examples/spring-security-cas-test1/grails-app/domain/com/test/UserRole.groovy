b253fb4be9a4d53649a89a76462ff590edf572f4
Mattias Reichel <mattias.reichel@gmail.com>
2025-02-14 15:31:54 +0100

    build: enable the `cas` projects

--- plugin-cas/examples/spring-security-cas-test1/grails-app/domain/com/test/UserRole.groovy
+++ plugin-cas/examples/spring-security-cas-test1/grails-app/domain/com/test/UserRole.groovy
@@ -3,7 +3,7 @@ package com.test
 import grails.gorm.DetachedCriteria
 import groovy.transform.ToString
 
-import org.apache.commons.lang3.builder.HashCodeBuilder
+import org.codehaus.groovy.util.HashCodeHelper
 
 @ToString(cache=true, includeNames=true, includePackage=false)
 class UserRole implements Serializable {
@@ -30,10 +30,14 @@ class UserRole implements Serializable {
 
 	@Override
 	int hashCode() {
-		def builder = new HashCodeBuilder()
-		if (user) builder.append(user.id)
-		if (role) builder.append(role.id)
-		builder.toHashCode()
+		int hashCode = HashCodeHelper.initHash()
+		if (user) {
+			hashCode = HashCodeHelper.updateHash(hashCode, user.id)
+		}
+		if (role) {
+			hashCode = HashCodeHelper.updateHash(hashCode, role.id)
+		}
+		hashCode
 	}
 
 	static UserRole get(long userId, long roleId) {
