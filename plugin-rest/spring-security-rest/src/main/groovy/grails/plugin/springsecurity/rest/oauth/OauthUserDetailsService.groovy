c597582ba89999846c73d672d2fd82bfd61496a0
Mattias Reichel <mattias.reichel@gmail.com>
2025-02-21 12:49:05 +0100

    build: enable `rest` plugin

--- plugin-rest/spring-security-rest/src/main/groovy/grails/plugin/springsecurity/rest/oauth/OauthUserDetailsService.groovy
+++ plugin-rest/spring-security-rest/src/main/groovy/grails/plugin/springsecurity/rest/oauth/OauthUserDetailsService.groovy
@@ -15,18 +15,17 @@
 package grails.plugin.springsecurity.rest.oauth
 
 import org.pac4j.core.profile.UserProfile
-import org.pac4j.oauth.profile.OAuth20Profile
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 
 /**
- * Load users based on a {@link OAuth20Profile}
+ * Load users based on a OAuth20Profile
  */
 interface OauthUserDetailsService extends UserDetailsService {
 
     /**
-     * Search for a user based on his {@link OAuth20Profile}. Implementations have a chance here to define additional
+     * Search for a user based on his OAuth20Profile. Implementations have a chance here to define additional
      * checks, like verifying that user's email domain is valid.
      *
      * @param userProfile user's OAuth profile returned by OAuth provider.
